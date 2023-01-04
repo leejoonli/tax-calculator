a59dd6883a3dc159e9f0f1883f98a8d328c36c61
jeremiahlee94 <jhwlee94@gmail.com>
2023-01-04 13:02:20 -0800

    add initial router code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
 root.render(
-    <App />
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
